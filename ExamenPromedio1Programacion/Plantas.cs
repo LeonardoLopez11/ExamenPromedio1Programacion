617afc6143f31804e1db2a2ecbc39a9b094178af
LeonardoLopez11 <85042599+LeonardoLopez11@users.noreply.github.com>
2023-09-26 21:03:55 -0500

    creacion de clase planta

--- ExamenPromedio1Programacion/Plantas.cs
+++ ExamenPromedio1Programacion/Plantas.cs
@@ -8,5 +8,43 @@ namespace ExamenPromedio1Programacion
 {
     class Plantas
     {
+        protected string Nombre;
+        protected int TiempoVida;
+        protected int CantidadProducto;
+        protected float SemillaValor;
+        protected float ProductoValor;
+        private int CrecimientoPlanta;
+
+        public Plantas(string nombre, int tiempoVida, int cantidadProducto, float semillaValor, float productoValor)
+        {
+            this.Nombre = nombre;
+            this.TiempoVida = tiempoVida;
+            this.CantidadProducto = cantidadProducto;
+            this.SemillaValor = semillaValor;
+            this.ProductoValor = productoValor;
+
+        }
+
+        public void CosecharPlanta()
+        {
+            if (CrecimientoPlanta <= 1)
+            {
+                Console.WriteLine("Has cosechado la planta demasiado pronto, no has obtenido su producto");
+            }
+            else if (CrecimientoPlanta == 2)
+            {
+                Console.WriteLine("Has cosechado la planta antes de tiempo, pero has obtenido 1 producto");
+            }
+            else if (CrecimientoPlanta == 3)
+            {
+                Console.WriteLine("Has cosechado la planta en el mejor momento, has obtenido 3 producto");
+            }
+            else
+            {
+
+                Console.WriteLine("Has cosechado la planta demasiado tarde, tanto que sus productos se han podrido")
+                    ;
+            }
+        }
     }
 }
