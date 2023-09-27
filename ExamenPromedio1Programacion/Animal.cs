f7d51648e94b23638399e8d31dc96903fbe722e9
LeonardoLopez11 <85042599+LeonardoLopez11@users.noreply.github.com>
2023-09-26 21:06:01 -0500

    creacion de clase animal y granja

---
+++ ExamenPromedio1Programacion/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPromedio1Programacion
+{
+    class Animal
+    {
+        protected string Nombre;
+        protected int TiempoVida;
+        protected float cantidadProducto;
+        protected float semillaValor;
+        protected float productoValor;
+
+
+        public Animal(string nombre, int tiempoVida, int cantidadProducto, float semillaValor, float productoValor)
+        {
+            this.Nombre = nombre;
+            this.TiempoVida = tiempoVida;
+            this.cantidadProducto = cantidadProducto;
+            this.semillaValor = semillaValor;
+            this.productoValor = productoValor;
+
+        }
+    }
+}
---
+++ ExamenPromedio1Programacion/Granja.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPromedio1Programacion
+{
+    class Granja
+    {
+        private int PrecioExpansion;
+        List<Animal> animales = new List<Animal>();
+        Animal Cerdo = new Animal("CERDO", 500, 50, 10, 50);
+        Animal Vaca = new Animal("VACA", 500, 100, 15, 10);
+
+        public void ExpandirGranja()
+        {
+            PrecioExpansion = PrecioExpansion + 10;
+            Console.WriteLine("Se ha completado la compra");
+        }
+    }
+}
